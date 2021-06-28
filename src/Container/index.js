9f49260c4e91eba016cabedbfaf4ea6be1c62023
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-06-28 20:54:25 +0200

    change name from Container to container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,9 @@
 import "./style.css"
 
-const Container = ({ children }) => (
-    <main className="Container">
+const container = ({ children }) => (
+    <main className="container">
         {children}
     </main>
 )
 
-export default Container
+export default container
