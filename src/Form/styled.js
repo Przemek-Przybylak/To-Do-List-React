4924c8ab1f25b8bf4675dc6bd21bb280c9e85f0c
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-13 21:07:57 +0200

    Create new styled components for Form

---
+++ src/Form/styled.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    @media (max-width: 767px) {
+        flex-wrap: wrap;
+        align-items: center;
+    };
+`;
+
+export const StyledButton = styled.button`
+    height: 40px;
+    margin-left: 15px;
+    margin-right: 15px;
+    width: 150px;
+    background-color: teal;
+    color: white;
+    border: none;
+    transition: 0.5s;
+
+    @media (max-width:767px) {
+        align-self: center;
+        margin-bottom: 20px;
+        width: 90%;
+        margin-right: auto;
+        margin-left: auto;
+    };
+
+    &:hover{
+        background-color: rgb(56, 179, 179);
+        transform: scale(1.1);
+    };
+`;
+
+export const StyledInput = styled.input`
+    font-size: 20px;
+    width: 100%;
+    padding: 10px;
+    height: 40px;
+    border: 1px solid rgb(214, 214, 214);
+
+    @media (max-width:767px) {
+        margin-right: 5%;
+        margin-left: 5%;
+        width: 90%;
+    };
+`;
+
+export const StyledParagraph = styled.p`
+    flex-basis: 100%;
+    padding: 0px 0px 0px 20px;
+`;
