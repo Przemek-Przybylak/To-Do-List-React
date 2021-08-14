6fe0bc4c80ac037dc9f0d63bf19b9caffbd32e2d
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-14 21:35:48 +0200

    create styled for  Container

---
+++ src/Container/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    background-color: #eee;
+    font-family: 'Ubuntu', sans-serif;
+    padding: 50px 300px;
+    height: 100vh;
+    box-sizing: border-box;
+
+@media (max-width: 996px) {
+
+        padding: 30px 20px;
+    };
+
+&*, ::after, ::before {
+    box-sizing: inherit;
+};
+`;
