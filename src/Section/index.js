8c43fdc1b371f6e79b3265d203ee5af2572415bc
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-14 22:40:04 +0200

    added styled for Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,13 +1,13 @@
-import "./style.css";
+import { SectionTittle, StyledSection, StyledSectionContainer } from "./Styled"
 
 const Section = ({ tittle, body, extraHeaderContent }) => (
-    <section className="section section__tasks">
-        <div className="section__container section__container--tittleTask">
-            <h2 className="section__tittle">{tittle}</h2>
+    <StyledSection className="section section__tasks">
+        <StyledSectionContainer className="section__container section__container--tittleTask">
+            <SectionTittle className="section__tittle">{tittle}</SectionTittle>
             {extraHeaderContent}
-        </div>
+        </StyledSectionContainer>
         {body}
-    </section>
+    </StyledSection>
 )
 
 export default Section
