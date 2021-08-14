95ee64e778ef3ed1368ac1d5975dead3cc47d433
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-14 22:40:47 +0200

    Create styled components for Section

---
+++ src/Section/Styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+    margin: 20px auto;
+    background-color: white;
+`;
+
+export const StyledSectionContainer = styled.div`
+    border-bottom: 1px solid rgb(214, 214, 214);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 767px) {
+        flex-wrap: wrap;
+        justify-content: center;
+    };
+`;
+
+export const SectionTittle = styled.h2`
+    padding: 30px;
+    font-size: 22px;
+    margin-bottom: 0;
+    margin-top: 0px;
+
+    @media (max-width: 767px) {
+        width: 100%;
+};
+`;
