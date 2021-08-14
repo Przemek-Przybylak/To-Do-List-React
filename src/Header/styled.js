0c93a1a9ba51a8f6b0c79f24a18fede01451736e
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-14 22:10:03 +0200

    create new styled components to Header component

---
+++ src/Header/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    margin-bottom: 25px;
+    font-size: 16px;
+`;
+
+export const StyledHeaderTittle = styled.h1`
+    padding-top: 20px;
+    padding-bottom: 10px;
+
+    @media(max-width: 996px){
+        padding-top: 5px;
+        padding-bottom: 3px;
+    };
+`;
