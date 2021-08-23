2a17d94efd8a5c8a8d5e93e242e56464d1a9e2fc
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-23 14:12:30 +0200

    added focus after add tasks

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { StyledForm, StyledButton, StyledInput, StyledParagraph } from "./styled";
 
 const Form = ({ addNewTask }) => {
 
     const [content, setcontent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(content.trim());
         setcontent("");
+        inputRef.current.focus();
     };
 
     return (
@@ -16,6 +18,7 @@ const Form = ({ addNewTask }) => {
             <StyledParagraph>
                 <label>
                     <StyledInput
+                        ref={inputRef}
                         value={content}
                         onChange={({ target }) => setcontent(target.value)}
                         placeholder="Co jest do zrobienia?"
