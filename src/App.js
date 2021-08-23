8bb093c6a0ae98c5c80c10e976e83ca9ceb48709
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-23 21:18:19 +0200

    useTasks hook added

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useTasks } from "./useTasks";
 
 function App() {
   const [hideDone, setHideDone] = useState(false)
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  const addNewTask = (content) => {
-
-    if (content === "")
-      return "";
-
-    (setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ])
-    )
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => {
-      return {
-        ...task,
-        done: true,
-      }
-    }));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }
-    ));
-  };
-
-  const removeTasks = (id) => {
-    setTasks(tasks => tasks.filter(
-      task => task.id !== id));
-  };
+  const {
+    tasks,
+    removeTasks,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone)
