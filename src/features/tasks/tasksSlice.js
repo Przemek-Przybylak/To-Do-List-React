02715c4087267bdde083cc163d70839a4e51f90a
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-05 19:06:49 +0200

    create setAllDone, removeTask reducers

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -4,7 +4,7 @@ const taskSlice = createSlice({
     name: 'tasks',
     initialState: {
         tasks: [],
-        hideDone: true,
+        hideDone: false,
     },
     reducers: {
         addTask: ({tasks}, {payload}) => {
@@ -17,8 +17,17 @@ const taskSlice = createSlice({
             const index = state.tasks.findIndex(task => task.id === action.payload);
             state.tasks[index].done = !state.tasks[index].done;
         },
+        setAllDone: state => {
+            state.tasks.forEach(task => {
+                task.done = true;
+            });
+        },
+        removeTask: (state, action) => {
+            const removeIndex = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks.splice(removeIndex, +1);
+        },
     },
 })
-export const { addTask, toggleHideDone, toggleTaskDone }= taskSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, setAllDone, removeTask }= taskSlice.actions;
 export const selectTasks = state => state.tasks;
 export default taskSlice.reducer;
