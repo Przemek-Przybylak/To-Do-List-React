650e1c4723d8e0852748c15663a6200d360f4957
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-05 19:08:03 +0200

    remove useless functions in Tasks

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,24 +1,11 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
 import Form from "./Form";
 import TasksList from "./TasksList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { useTasks } from "../../useTasks";
-import { selectTasks } from "./tasksSlice";
 
 export function Tasks() {
-  const { tasks } = useSelector(selectTasks);
-
-  const {
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
-
 
   return (
     <Container>
@@ -27,18 +14,18 @@ export function Tasks() {
       />
       <Section
         tittle="Dodaj nowe zadanie"
-        body={< Form addNewTask={addNewTask} />}
+        body={
+          < Form />
+        }
       />
       <Section
         tittle="Lista zadań"
         body={
-          <TasksList
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />}
-        extraHeaderContent={
-          <Buttons
-            setAllDone={setAllDone} />}
+          <TasksList />
+        }
+        extraHeaderContent={ 
+          <Buttons />
+        }
       />
     </Container>
   );
