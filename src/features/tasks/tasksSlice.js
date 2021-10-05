a2cf55986f3dba988ad4f23706550f600122bb28
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-05 17:33:13 +0200

    create toggleTaskDone reducer in taskSlice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -4,17 +4,21 @@ const taskSlice = createSlice({
     name: 'tasks',
     initialState: {
         tasks: [],
-        hideDone: false,
+        hideDone: true,
     },
     reducers: {
         addTask: ({tasks}, {payload}) => {
             tasks.push(payload)
         },
-        toggleHideDone: (state, action) => {
+        toggleHideDone: state => {
             state.hideDone= !state.hideDone;
+        },
+        toggleTaskDone: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks[index].done = !state.tasks[index].done;
         },
     },
 })
-export const { addTask, toggleHideDone }= taskSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone }= taskSlice.actions;
 export const selectTasks = state => state.tasks;
 export default taskSlice.reducer;
