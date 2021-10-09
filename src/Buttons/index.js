6b9bdaa7d95925f206d9986d7a720c61088ca46c
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-09 18:19:08 +0200

    transfer Buttons to src

---
+++ src/Buttons/index.js
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+import { ButtonItem } from "./styled"
+import { selectTasks, toggleHideDone, setAllDone } from "../../src/features/tasks/tasksSlice";
+
+const Buttons = () => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+
+  if (tasks.lenght === 0)
+    return null;
+
+  return (
+    <div>
+      <ButtonItem
+        onClick={() => dispatch(toggleHideDone())}
+        disabled={tasks.every(({ done }) => done)}
+      >
+        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+      </ButtonItem>
+      <ButtonItem
+        onClick={() => dispatch(setAllDone())}
+        disabled={tasks.every(({ done }) => done)}
+      >
+        Ukończ wszystkie
+      </ButtonItem>
+    </div>
+  );
+};
+
+export default Buttons
---
+++ src/Buttons/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ButtonItem = styled.button`
+    border: none;
+    color: teal;
+    padding: 20px;
+    background-color: white;
+    transition: 0.5s;
+
+    @media (max-width: 767px) {
+    width: 100%;
+    };
+
+    &:hover{
+    color: rgb(56, 179, 179);
+    cursor: pointer;
+    };
+
+    &:disabled{
+    color: #ccc;
+    };
+`;
