aadf1a4e31166b7851ba28acf6f93640b297925c
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-16 19:52:17 +0200

    added router to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
-import{ Tasks }from "./features/tasks/Tasks";
+import { BrowserRouter, Link, Switch, Route, BroserRouter } from "react-router-dom";
+import { Tasks } from "./features/tasks/Tasks";
+import { Author } from "./features/author/Author";
 
-export const App = () => <Tasks />;
+export const App = () =>
+    <BrowserRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">Zadania</Link>
+                    <Link to="/author">O autorze</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/author">
+                    <Author />
+                </Route>
+            </Switch>
+        </nav>
+    </BrowserRouter>
+    ;
 
