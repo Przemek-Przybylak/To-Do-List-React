aa0db31e6829dc0a955019c79a71139f8ec85ba0
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-17 19:20:37 +0200

    create style for nav list

---
+++ src/features/NavList/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Ul = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: teal;
+    color: white;
+    margin: 0px;
+    padding: 0px;
+`;
+
+export const Li = styled.li`
+    list-style-type: none;
+    padding: 5px;
+    font-size: 23px;
+`;
+
+export const Span = styled.span`
+    text-decoration: none;
+    color: white;
+`;
