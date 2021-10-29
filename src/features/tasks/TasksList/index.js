d700dad14b0ca2c5d35a09d0ecb3d89127c0e553
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-29 08:06:19 +0200

    delete useless space in TasksList

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -2,7 +2,6 @@ import { List, Item, Button, Content } from "./styled";
 import { selectTaskState, toggleTaskDone, removeTask } from "../tasksSlice";
 import { useSelector, useDispatch } from "react-redux";
 
-
 const TasksList = () => {
     const { tasks, hideDone } = useSelector(selectTaskState);
     const dispatch = useDispatch();
