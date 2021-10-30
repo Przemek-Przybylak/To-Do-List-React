990da3c003805cf22a5e93f52e203d04406db429
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-10-30 14:21:37 +0200

    added Wrapper, remove Paragraph in Search

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,15 +1,16 @@
 import { useHistory, useLocation } from "react-router";
-import { Paragraph, Input } from "../Form/styled"
+import { Input } from "../Form/styled"
+import { Wrapper } from "./styled";
 
 export default () => {
     const location = useLocation();
     const history = useHistory();
     const query = (new URLSearchParams(location.search)).getAll("szukaj");
-    
+
     const onInputChange = ({ target }) => {
         const searchParams = new URLSearchParams(location.search);
 
-        if(target.value.trim() === "") {
+        if (target.value.trim() === "") {
             searchParams.delete("szukaj");
         } else {
             searchParams.set("szukaj", target.value);
@@ -17,14 +18,14 @@ export default () => {
 
         history.push(`${location.pathname}?${searchParams.toString()}`)
     };
-    
+
     return (
-        <Paragraph>
-            <Input
-            placeholder="Filtruj zadania"
-            value={query || ""}
-            onChange={onInputChange}
-            />
-        </Paragraph>
+        <Wrapper>
+                <Input
+                    placeholder="Filtruj zadania"
+                    value={query || ""}
+                    onChange={onInputChange}
+                />
+        </Wrapper>
     )
 }
