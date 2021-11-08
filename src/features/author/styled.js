d81eaaa0a0b436a79498eae537f17455598bfd2d
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-08 17:44:52 +0100

    added styles to AuthorPage Paragraph

--- src/features/author/styled.js
+++ src/features/author/styled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
-
 export const Article = styled.article`
     padding: 30px;
     font-size: 100%;
     line-height: 1.15;
     letter-spacing: 0.3px;
+`;
+
+export const Paragraph = styled.p`
+    margin: 0px 0px 15px 0px;
 `;
